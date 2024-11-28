4533a4b9efca3993810768e32d3c7d68777a1fd9
Çağatay Kağan Atalay <44618147+Ckatalay@users.noreply.github.com>
2024-11-28 17:50:40 +0300

    fix

--- Assignment_9/q2/q2.c
+++ Assignment_9/q2/q2.c
@@ -7,7 +7,7 @@ char *get_string(char *prompt);
 
 int main(void)
 {
-    char *text = get_string("Please input a string: ");
+    char *text = "happynewyear";
     char val = get_char("Please input a character to count: ");
 
     printf("The character '%c' appears %d times in text.\n", val, count(text, val));
@@ -17,7 +17,7 @@ int count(char *string, char goal)
 {
     int count = 0;
 
-    for (int i = 0; string[i] != '\n'; i++)
+    for (int i = 0; string[i] != '\0'; i++)
     {
         if (string[i] == goal)
         {
@@ -47,12 +47,4 @@ char get_char(char *prompt)
         }
     }
     return c;
-}
-
-char *get_string(char *prompt)
-{
-    char *input = malloc(256);
-    printf("%s", prompt);
-    fgets(input, 256, stdin);
-    return input;
 }
